61ace77ee56cd1b10e1c24ad0dbd5df6a50f4d6b
Naman-3010 <91064945+Naman-3010@users.noreply.github.com>
2022-07-08 15:25:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseX=0;
+noseY=0;
 
+function preload(){
+    Mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
@@ -16,8 +19,11 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("nose x="+results[0].pose.lips.x);
-        console.log("nose y="+results[0].pose.lips.y);
+        noseX=results[0].pose.nose.x-30;
+        noseY=results[0].pose.nose.y;
+
+        console.log("nose x="+noseX);
+        console.log("nose y="+noseY);
     }
 }
 
@@ -27,6 +33,7 @@ function modelLoaded(){
 
 function draw(){
 image(video,0,0,300,300);
+image(Mustache,noseX,noseY,60,40);
 }
 
 function take_snapshot(){
